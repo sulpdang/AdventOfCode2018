e8ae1aa6680ae69d03669640bfebd80126d52663
Yohho Kim <sulpdang@gmail.com>
2018-12-08 20:56:41 +0900

    Prob Solved (Using Tree) v3

--- day08/src/main/scala/Main.scala
+++ day08/src/main/scala/Main.scala
@@ -14,20 +14,9 @@ trait Tree {
   val sum:Int
 }
 
-trait Node extends Tree {
-  val child:List[Tree]
-}
-
-case class JustSumNode(meta:List[Int], child:List[Tree]) extends Node {
-  val sum:Int = meta.sum + child.map{_.sum}.sum
-}
-case class ChildSumNode(meta:List[Int], child:List[Tree]) extends Node {
-  val sum:Int = {
-    val childValues = child.map{_.sum}
-    meta.map{ m => 
-      if(m <= child.size) Some(childValues(m-1)) else None
-    }.flatten.sum
-  }
+case class Node(meta:List[Int], child:List[Tree], 
+  sumFunc:(List[Int], List[Tree]) => Int) extends Tree {
+  val sum:Int = sumFunc(meta, child)
 }
 case class Leaf(val meta:List[Int]) extends Tree {
   val sum:Int = meta.sum
@@ -38,7 +27,7 @@ object Main extends Day(8) {
   def processedInput = {
     input.head.split(' ').map{_.toInt}.toList
   }
-  def makingTree(input:List[Int])(makingNode:(List[Int], List[Tree]) => Node):Tree = {
+  def makingTree(input:List[Int])(sumNode:(List[Int], List[Tree]) => Int):Tree = {
     def makingTreeAcc(remainInput:List[Int]):(List[Int], Tree) = {
       remainInput match {
         case a :: b :: tail if a == 0 => (tail.drop(b), Leaf(tail.take(b)))
@@ -53,15 +42,20 @@ object Main extends Day(8) {
             }
           }
           val (nRemain, childs) = times(a, tail)
-          (nRemain.drop(b), makingNode(nRemain.take(b), childs))
+          (nRemain.drop(b), Node(nRemain.take(b), childs, sumNode))
       }
     }
     makingTreeAcc(input)._2
   }
   def solve(input:Input) = {
-    makingTree(input){case (meta, childs) => JustSumNode(meta,childs) }.sum
+    makingTree(input){case (meta, child) => meta.sum + child.map{_.sum}.sum }.sum
   }
   def solve2(input:Input) = {
-    makingTree(input){case (meta, childs) => ChildSumNode(meta,childs) }.sum
+    makingTree(input){case (meta, child) => 
+      val childValues = child.map{_.sum}
+      meta.map{ m => 
+        if(m <= child.size) Some(childValues(m-1)) else None
+      }.flatten.sum
+    }.sum
   }
 }
